b15f997524ff2758c5c7ad190c88fe231ecba566
oak45985 <oak45985@gmail.com>
2022-10-25 19:02:13 -0500

    everything set up for front end and servers

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -6,14 +7,25 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 
 function App() {
+  const httpLink = createHttpLink({
+    uri: '/graphql',
+  });
+  
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>  
   );
 }
 
